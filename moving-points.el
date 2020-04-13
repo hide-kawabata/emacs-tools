98567ff63840df0135329f6214c56782a7f9982f
Hideyuki Kawabata <hide.kawabata@gmail.com>
2020-04-13 19:02:49 +0900

    Modify the behavior of C-e.

--- moving-points.el
+++ moving-points.el
@@ -6,5 +6,8 @@
 (defun move-end-of-screen-line ()
   "Move point to the end of current screen line."
   (interactive
-    (progn (vertical-motion 1)
-	   (backward-char))))
+   (progn
+     (setq a (progn (vertical-motion 0) (point)))
+     (setq b (progn (vertical-motion 1) (vertical-motion 0) (point)))
+     (if (= a b) (interactive-form (vertical-motion 1))
+       (progn (backward-char))))))
